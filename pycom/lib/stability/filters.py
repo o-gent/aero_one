62b172e77bea63305d8df14c8a7d4b32ee09ff36
Nethereel <39216715+Nethereel@users.noreply.github.com>
2019-05-11 13:44:25 +0100

    Update filters.py

--- pycom/lib/stability/filters.py
+++ pycom/lib/stability/filters.py
@@ -13,3 +13,17 @@ class LowPassFilter:
         self.output = (1-a) * self.out_prev + a * input_ * self.amp
         self.out_prev = self.output
         return self.output
+
+class WashoutFilter:
+    def __init__(self, tau):
+        self.tau = tau
+        self.output = 0
+        self.out_prev = 0
+        self.in_prev = 0
+
+    def step(self, input_, dt):
+        a = self.tau / (self.tau + dt)
+        self.output = a*self.out_prev + a*input_ - self.in_prev)
+        self.out_prev = self.output
+        self.in_prev = input_
+        return self.output
